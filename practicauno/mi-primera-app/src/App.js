82982f8f7dc4441f4631a94598d8386ece55e83e
Alba2024 <155449293+Alba2024@users.noreply.github.com>
2024-06-11 15:28:05 +0000

    miercoles

---
+++ practicauno/mi-primera-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Evento from './Componentes/Evento.js';
+function App() {
+    return(
+        <div className="App">
+            <h1>Evento</h1>
+            <Evento/>
+        </div>
+    );
+}
+export default App;
